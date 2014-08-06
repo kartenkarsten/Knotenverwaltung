c53eed21d4102877f3bd4c1b22fcc52b2a5e6866
arnimw <arnim@posteo.de>
2014-08-06 09:14:49 +0200

    Updates in database schema

--- fflg-knoten.sql
+++ fflg-knoten.sql
@@ -1,13 +1,4 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE "knoten" (
-    "knoten_id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    "name" TEXT,
-    "firstname" TEXT,
-    "lastname" TEXT,
-    "email" TEXT,
-    "key" TEXT,
-    "location" TEXT,
-    "edit" INTEGER
-);
+CREATE TABLE knoten (knoten_id INTEGER PRIMARY KEY, name TEXT, firstname TEXT, lastname TEXT, email TEXT, key TEXT, location TEXT, edit INTEGER, new_date TEXT, last_seen TEXT, delhash TEXT);
 COMMIT;
